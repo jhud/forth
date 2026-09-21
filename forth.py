07d06cc4897c4945214810c33ce98fff105c30f3
James Hudson <bugs.feedback.whatever@gmail.com>
2026-09-21 10:02:10 +0300

    Basic function storage.

--- forth.py
+++ forth.py
@@ -9,10 +9,15 @@ def popnum():
 
 stack = []
 dictionary = {
+    # Arithmetic operators
     "+": lambda: stack.append(popnum() + popnum()), 
     "-": lambda: stack.append(popnum() - popnum()), 
     "*": lambda: stack.append(popnum() * popnum()), 
     "/": lambda: stack.append(popnum() / popnum()),
+
+    # Inbuilt debug / quit functions
+    "_q": lambda: exit(),
+    "_d": lambda: print(dictionary),
     }
 
 
@@ -26,7 +31,9 @@ def process_tokens(tokens):
     """ Process a string and produce an output, with stack manipulation."""
     first_token = tokens[0]
     if first_token == ":":
-        dictionary[tokens[1]] = tokens[2:token_index(tokens[2:], ";")]
+        code_definition_end = token_index(tokens[2:], ";")+2
+        dictionary[tokens[1]] = tokens[2:code_definition_end]
+        return tokens[code_definition_end+1:]
     elif first_token.isnumeric():
         stack.append(first_token)
     else:
@@ -34,7 +41,9 @@ def process_tokens(tokens):
         if callable(found):
             found()
         else:
-            process_tokens(dictionary[first_token])
+            found_tokens = dictionary[first_token]
+            while len(found_tokens):
+                found_tokens = process_tokens(found_tokens)
     return tokens[1] if len(tokens) > 1 else []
 
 def repl():
@@ -42,8 +51,6 @@ def repl():
     print("Launching REPL...")
     while True:
         data = input("* ")
-        if data == "\\quit":
-            break
         try:
             tokens = data.split(" ")
             while len(tokens):
@@ -51,7 +58,7 @@ def repl():
             print(stack)
         except Exception as e:
             print(f"ERROR: {e}")
-            #raise e # Uncomment to use the Python debugger for tracing errors
+            raise e # Uncomment to use the Python debugger for tracing errors
 
 if __name__ == '__main__':
     """ Try evaluating some expressions."""
